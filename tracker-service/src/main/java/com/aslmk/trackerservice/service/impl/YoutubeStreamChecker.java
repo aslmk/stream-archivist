cb49840e69364cd7132a0759241b7faa835b5a1e
aslmk <aulaensov@gmail.com>
2025-07-02 13:03:03 +0500

    feat: use YoutubeApiClient to check live status in YoutubeStreamChecker
    - Inject YoutubeApiClient into YoutubeStreamChecker
    - Delegate isLive check to YoutubeApiClient

--- tracker-service/src/main/java/com/aslmk/trackerservice/service/impl/YoutubeStreamChecker.java
+++ tracker-service/src/main/java/com/aslmk/trackerservice/service/impl/YoutubeStreamChecker.java
@@ -1,12 +1,20 @@
 package com.aslmk.trackerservice.service.impl;
 
 import com.aslmk.trackerservice.service.PlatformStreamChecker;
+import com.aslmk.trackerservice.youtube.YoutubeApiClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class YoutubeStreamChecker implements PlatformStreamChecker {
+
+    private final YoutubeApiClient youtubeApiClient;
+
+    public YoutubeStreamChecker(YoutubeApiClient youtubeApiClient) {
+        this.youtubeApiClient = youtubeApiClient;
+    }
+
     @Override
-    public boolean isLive(String streamerName) {
-        return false;
+    public boolean isLive(String broadcasterUsername) {
+        return youtubeApiClient.isChannelLive(broadcasterUsername);
     }
 }
