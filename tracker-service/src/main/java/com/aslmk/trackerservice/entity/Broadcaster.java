1b0e8ccc60e62ce77d6da25bf43294ee576eb0d9
aslmk <aulaensov@gmail.com>
2025-07-02 13:08:13 +0500

    feat: update Broadcaster entity with username and platform fields
    - add username and platform fields to store username and platform of the broadcaster

--- tracker-service/src/main/java/com/aslmk/trackerservice/entity/Broadcaster.java
+++ tracker-service/src/main/java/com/aslmk/trackerservice/entity/Broadcaster.java
@@ -1,10 +1,15 @@
 package com.aslmk.trackerservice.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Broadcaster {
-
+    private String username;
+    private String platform;
 }
