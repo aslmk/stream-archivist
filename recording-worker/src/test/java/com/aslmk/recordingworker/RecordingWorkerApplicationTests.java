2797adfcb2a0558b527e3603deee24f35a75d1e1
aslmk <aulaensov@gmail.com>
2025-07-25 18:11:20 +0500

    chore: add missing files to recording-worker module

---
+++ recording-worker/src/test/java/com/aslmk/recordingworker/RecordingWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aslmk.recordingworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordingWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
