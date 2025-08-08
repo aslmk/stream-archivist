6977133d2426af6cc3ab1919d503bc03c277893f
aslmk <aulaensov@gmail.com>
2025-08-08 13:25:09 +0500

    feat: implement basic orchestrator-worker interaction via REST API
    - orchestrator accepts HTTP request with recording params
    - directly forwards task to recording-worker using RestClient

---
+++ recording-orchestrator-service/src/test/java/com/aslmk/recordingorchestratorservice/RecordingOrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aslmk.recordingorchestratorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordingOrchestratorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
