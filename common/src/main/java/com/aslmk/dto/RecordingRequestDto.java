de77b40b01abf2ede4bf3e404bb36554dc4622e3
aslmk <aulaensov@gmail.com>
2025-08-15 15:59:55 +0500

    chore: add common module with shared DTO

---
+++ common/src/main/java/com/aslmk/dto/RecordingRequestDto.java
@@ -0,0 +1,22 @@
+package com.aslmk.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecordingRequestDto {
+    private String streamerUsername;
+    private String streamUrl;
+    private String streamQuality;
+
+    @Override
+    public String toString() {
+        return "StreamerDto [" +
+                "streamerUsername=" + streamerUsername +
+                ";\n streamUrl=" + streamUrl +
+                ";\n streamQuality=" + streamQuality + "]";
+    }
+}
