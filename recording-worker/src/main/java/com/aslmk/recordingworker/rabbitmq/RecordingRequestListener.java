8f57902af9afa0f10f0c205254929835a01d16c3
aslmk <aulaensov@gmail.com>
2025-08-17 15:56:53 +0500

    refactor: use StreamRecorderService in RabbitMQ listener
    - update RecordingRequestListener to delegate to StreamRecorderService
    - replace previous REST-based call with RabbitMQ-driven flow

--- recording-worker/src/main/java/com/aslmk/recordingworker/rabbitmq/RecordingRequestListener.java
+++ recording-worker/src/main/java/com/aslmk/recordingworker/rabbitmq/RecordingRequestListener.java
@@ -1,6 +1,7 @@
 package com.aslmk.recordingworker.rabbitmq;
 
 import com.aslmk.common.dto.RecordingRequestDto;
+import com.aslmk.recordingworker.service.StreamRecorderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class RecordingRequestListener {
 
+    public final StreamRecorderService recordingService;
+
+    public RecordingRequestListener(StreamRecorderService recordingService) {
+        this.recordingService = recordingService;
+    }
+
     @RabbitListener(queues = "${user.rabbitmq.queue.name}")
     public void handleRecordingRequest(RecordingRequestDto request) {
         log.info("Received request: \n{}", request);
+        recordingService.recordStream(request);
     }
 }
