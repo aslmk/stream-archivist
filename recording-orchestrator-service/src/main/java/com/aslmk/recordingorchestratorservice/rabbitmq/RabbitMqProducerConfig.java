fda91f0581279763e86c2804b6a9441952a1a89d
aslmk <aulaensov@gmail.com>
2025-08-17 12:59:30 +0500

    feat: set up basic RabbitMQ producer
    Introduce producer for orchestrator -> worker communication
    
    - configure RabbitMQ connection properties in application.properties
    - add RabbitMQ dependency to pom.xml
    - use RecordingRequestDto from common module
    - introduce RabbitMqService to send tasks to the queue

---
+++ recording-orchestrator-service/src/main/java/com/aslmk/recordingorchestratorservice/rabbitmq/RabbitMqProducerConfig.java
@@ -0,0 +1,35 @@
+package com.aslmk.recordingorchestratorservice.rabbitmq;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqProducerConfig {
+
+    @Value("${user.rabbitmq.queue.name}")
+    private String queueName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
---
+++ recording-orchestrator-service/src/main/java/com/aslmk/recordingorchestratorservice/rabbitmq/RabbitMqService.java
@@ -0,0 +1,27 @@
+package com.aslmk.recordingorchestratorservice.rabbitmq;
+
+import com.aslmk.common.dto.RecordingRequestDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class RabbitMqService {
+
+    @Value("${user.rabbitmq.queue.name}")
+    private String queueName;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitMqService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(RecordingRequestDto message) {
+        log.info("sending message to RabbitMQs queue: {}", queueName);
+        log.info("The message: \n{}", message);
+        rabbitTemplate.convertAndSend(queueName, message);
+    }
+}
