b8f9903913628e9fec3f3d5e294c8290aa2cafc0
aslmk <aulaensov@gmail.com>
2025-08-24 13:05:39 +0500

    refactor: remove JPA and scheduler
    - remove JPA dependency from pom.xml
    - delete Streamer entity, repository, service and controller
    - remove StreamCheckScheduler (legacy polling logic)

--- tracker-service/src/main/java/com/aslmk/trackerservice/service/StreamerService.java
+++
@@ -1,9 +0,0 @@
-package com.aslmk.trackerservice.service;
-
-import com.aslmk.trackerservice.dto.AddStreamerRequestDto;
-import com.aslmk.trackerservice.dto.DeleteStreamerRequestDto;
-
-public interface StreamerService {
-    void saveStreamer(AddStreamerRequestDto newBroadcaster);
-    void deleteStreamer(DeleteStreamerRequestDto deleteBroadcaster);
-}
