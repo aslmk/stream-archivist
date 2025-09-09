644b4402589750927405a7577d3bed90a5bdef06
aslmk <aulaensov@gmail.com>
2025-09-09 18:51:05 +0500

    feat(uploading-worker): implement file chunking logic
    - add FileSplitterService for file chunking
    - introduce FilePart DTO and FileSplittingException
    - update application.properties with chunking config

---
+++ uploading-worker/src/main/java/com/aslmk/uploadingworker/exception/FileSplittingException.java
@@ -0,0 +1,7 @@
+package com.aslmk.uploadingworker.exception;
+
+public class FileSplittingException extends RuntimeException {
+    public FileSplittingException(String message) {
+        super(message);
+    }
+}
