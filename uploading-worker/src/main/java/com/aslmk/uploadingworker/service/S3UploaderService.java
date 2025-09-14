8a3ab8db6b9629aa6e50174b614830b8760c5550
aslmk <aulaensov@gmail.com>
2025-09-14 15:28:01 +0500

    fix: update imports after moving PartUploadResultDto to common module

--- uploading-worker/src/main/java/com/aslmk/uploadingworker/service/S3UploaderService.java
+++ uploading-worker/src/main/java/com/aslmk/uploadingworker/service/S3UploaderService.java
@@ -1,6 +1,6 @@
 package com.aslmk.uploadingworker.service;
 
-import com.aslmk.uploadingworker.dto.PartUploadResultDto;
+import com.aslmk.common.dto.PartUploadResultDto;
 import com.aslmk.uploadingworker.dto.S3UploadRequestDto;
 
 import java.util.List;
