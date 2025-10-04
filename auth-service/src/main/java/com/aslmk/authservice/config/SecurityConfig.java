a98df1fca7612c9dd4331cc3377d58394882f1c8
aslmk <aulaensov@gmail.com>
2025-10-04 22:18:49 +0500

    feat(auth): add OAuth2LoginSuccessHandler to extract Twitch tokens
    - update SecurityConfig to wire custom handler
    
    TODO: store tokens in DB

--- auth-service/src/main/java/com/aslmk/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/aslmk/authservice/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.aslmk.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,10 +10,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final OAuthLoginSuccessHandler oAuthLoginSuccessHandler;
+
+    public SecurityConfig(OAuthLoginSuccessHandler oAuthLoginSuccessHandler) {
+        this.oAuthLoginSuccessHandler = oAuthLoginSuccessHandler;
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(oauth ->
+                        oauth.successHandler(oAuthLoginSuccessHandler))
                 .build();
     }
 }
