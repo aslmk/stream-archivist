e5499b002090a1791239c335210825ff294427ff
aslmk <aulaensov@gmail.com>
2025-10-21 15:46:50 +0500

    feat: add TokenUpdateService and extend TokenService to support token updates

--- auth-service/src/main/java/com/aslmk/authservice/service/TokenService.java
+++ auth-service/src/main/java/com/aslmk/authservice/service/TokenService.java
@@ -3,6 +3,11 @@ package com.aslmk.authservice.service;
 import com.aslmk.authservice.dto.CreateTokenDto;
 import com.aslmk.authservice.entity.TokenEntity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TokenService {
     TokenEntity create(CreateTokenDto dto);
+    List<TokenEntity> getExpiredTokens(LocalDateTime now);
+    void update(TokenEntity entity);
 }
--- auth-service/src/main/java/com/aslmk/authservice/service/impl/TokenServiceImpl.java
+++ auth-service/src/main/java/com/aslmk/authservice/service/impl/TokenServiceImpl.java
@@ -6,9 +6,11 @@ import com.aslmk.authservice.repository.TokenRepository;
 import com.aslmk.authservice.service.TokenService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 public class TokenServiceImpl implements TokenService {
-
     private final TokenRepository tokenRepository;
 
     public TokenServiceImpl(TokenRepository tokenRepository) {
@@ -25,5 +27,15 @@ public class TokenServiceImpl implements TokenService {
                 .build();
 
         return tokenRepository.save(tokenEntity);
+    }
+
+    @Override
+    public List<TokenEntity> getExpiredTokens(LocalDateTime now) {
+        return tokenRepository.findAllByExpiresAtBefore(now);
+    }
+
+    @Override
+    public void update(TokenEntity token) {
+        tokenRepository.save(token);
     }
 }
