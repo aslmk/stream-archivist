53f9f43205951834f4269bdda8f757a1a880ce8a
aslmk <aulaensov@gmail.com>
2025-10-25 22:38:11 +0500

    feat: refresh Twitch tokens for existing users during login

--- auth-service/src/main/java/com/aslmk/authservice/service/TokenUpdateService.java
+++ auth-service/src/main/java/com/aslmk/authservice/service/TokenUpdateService.java
@@ -1,5 +1,8 @@
 package com.aslmk.authservice.service;
 
+import com.aslmk.authservice.entity.TokenEntity;
+
 public interface TokenUpdateService {
     void updateExpiredTokens();
+    void updateIfExpired(TokenEntity token);
 }
