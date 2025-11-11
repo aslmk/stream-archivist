30e869b17e30d7b686b34fd9db0dceee5aa87efb
aslmk <aulaensov@gmail.com>
2025-11-11 16:41:32 +0500

    feat: add unit and integration tests for TrackingService and TwitchApiClient

---
+++ tracker-service/src/test/java/com/aslmk/trackerservice/TrackingServiceUnitTests.java
@@ -0,0 +1,117 @@
+package com.aslmk.trackerservice;
+
+import com.aslmk.trackerservice.dto.TrackingRequestDto;
+import com.aslmk.trackerservice.entity.StreamerEntity;
+import com.aslmk.trackerservice.exception.TrackingException;
+import com.aslmk.trackerservice.service.StreamerService;
+import com.aslmk.trackerservice.service.impl.TrackingServiceImpl;
+import com.aslmk.trackerservice.streamingPlatform.twitch.client.TwitchApiClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TrackingServiceUnitTests {
+
+    @Mock
+    private TwitchApiClient twitchClient;
+
+    @Mock
+    private StreamerService streamerService;
+
+    @InjectMocks
+    private TrackingServiceImpl trackingService;
+
+    private TrackingRequestDto validRequest;
+
+    private static final String STREAMER_USERNAME = "test0";
+    private static final String STREAM_QUALITY = "720p";
+    private static final String PROVIDER_NAME = "twitch";
+    
+    @BeforeEach
+    void setUp() {
+        validRequest = TrackingRequestDto.builder()
+                .streamerUsername(STREAMER_USERNAME)
+                .streamQuality(STREAM_QUALITY)
+                .providerName(PROVIDER_NAME)
+                .build();
+    }
+
+    @Test
+    void should_throwTrackingException_when_requestIsNull() {
+         Assertions.assertThrows(TrackingException.class,
+                () -> trackingService.trackStreamer(null));
+    }
+
+    @Test
+    void should_throwTrackingException_when_usernameIsBlank() {
+        validRequest.setStreamerUsername(" ");
+        Assertions.assertThrows(TrackingException.class,
+                () -> trackingService.trackStreamer(validRequest));
+    }
+
+    @Test
+    void should_throwTrackingException_when_providerNameIsBlank() {
+        validRequest.setProviderName(" ");
+        Assertions.assertThrows(TrackingException.class,
+                () -> trackingService.trackStreamer(validRequest));
+    }
+
+    @Test
+    void should_throwTrackingException_when_streamQualityIsBlank() {
+        validRequest.setStreamQuality(" ");
+        Assertions.assertThrows(TrackingException.class,
+                () -> trackingService.trackStreamer(validRequest));
+    }
+
+    @Test
+    void should_returnImmediately_when_streamerAlreadyTracked() {
+        Mockito.when(streamerService.findByUsername(STREAMER_USERNAME))
+                .thenReturn(Optional.of(new StreamerEntity()));
+
+        trackingService.trackStreamer(validRequest);
+
+        Mockito.verify(streamerService).findByUsername(STREAMER_USERNAME);
+
+        Mockito.verifyNoMoreInteractions(streamerService, twitchClient);
+    }
+
+    @Test
+    void should_updateUsername_when_streamerExistsByIdAndProvider() {
+        Mockito.when(streamerService.findByUsername(STREAMER_USERNAME)).thenReturn(Optional.empty());
+        Mockito.when(twitchClient.getStreamerId(STREAMER_USERNAME)).thenReturn("123");
+        Mockito.when(streamerService.findByProviderUserIdAndProviderName("123", PROVIDER_NAME))
+                .thenReturn(Optional.of(new StreamerEntity()));
+
+        trackingService.trackStreamer(validRequest);
+
+        Mockito.verify(streamerService).updateUsername(any(StreamerEntity.class), eq(STREAMER_USERNAME));
+        Mockito.verify(twitchClient, never()).subscribeToStreamer(anyString());
+        Mockito.verify(streamerService, never()).create(any());
+    }
+
+    @Test
+    void should_createStreamer_when_notTrackedAndNotInDb() {
+        Mockito.when(streamerService.findByUsername(STREAMER_USERNAME)).thenReturn(Optional.empty());
+        Mockito.when(twitchClient.getStreamerId(STREAMER_USERNAME)).thenReturn("123");
+        Mockito.when(streamerService.findByProviderUserIdAndProviderName("123", PROVIDER_NAME))
+                .thenReturn(Optional.empty());
+
+        trackingService.trackStreamer(validRequest);
+
+        Mockito.verify(twitchClient).subscribeToStreamer("123");
+        Mockito.verify(streamerService).create(argThat(dto ->
+                dto.getUsername().equals(STREAMER_USERNAME) &&
+                        dto.getStreamerId().equals("123") &&
+                        dto.getProviderName().equals(PROVIDER_NAME)));
+    }
+}
