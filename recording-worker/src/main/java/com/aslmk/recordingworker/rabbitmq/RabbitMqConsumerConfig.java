d2120eda04c4e06e2f14a6c185d4e7d8970fff36
aslmk <aulaensov@gmail.com>
2025-11-19 22:06:27 +0500

    fix: add automatic RabbitMQ queue creation in recording-worker to prevent startup crashes

--- recording-worker/src/main/java/com/aslmk/recordingworker/rabbitmq/RabbitMqConsumerConfig.java
+++ recording-worker/src/main/java/com/aslmk/recordingworker/rabbitmq/RabbitMqConsumerConfig.java
@@ -1,12 +1,22 @@
 package com.aslmk.recordingworker.rabbitmq;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConsumerConfig {
+
+    @Value("${user.rabbitmq.queue.name}")
+    private String queueName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
