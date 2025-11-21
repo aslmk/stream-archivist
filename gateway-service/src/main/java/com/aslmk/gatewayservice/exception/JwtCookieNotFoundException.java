9f86aa999a141fc024fdb33444730fc0d3bce064
aslmk <aulaensov@gmail.com>
2025-11-21 19:45:27 +0500

    chore: remove obsolete JwtCookieNotFoundException

--- gateway-service/src/main/java/com/aslmk/gatewayservice/exception/JwtCookieNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.aslmk.gatewayservice.exception;
-
-public class JwtCookieNotFoundException extends RuntimeException {
-    public JwtCookieNotFoundException(String message) {
-        super(message);
-    }
-}
