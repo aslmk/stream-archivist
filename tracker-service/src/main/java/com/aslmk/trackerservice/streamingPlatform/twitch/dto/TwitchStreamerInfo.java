802435317486569e86a076a96c68cd375de74c9e
aslmk <aulaensov@gmail.com>
2025-12-22 20:42:45 +0500

    refactor: remove obsolete fields from TwitchStreamerInfo

--- tracker-service/src/main/java/com/aslmk/trackerservice/streamingPlatform/twitch/dto/TwitchStreamerInfo.java
+++ tracker-service/src/main/java/com/aslmk/trackerservice/streamingPlatform/twitch/dto/TwitchStreamerInfo.java
@@ -13,20 +13,6 @@ import lombok.*;
 public class TwitchStreamerInfo {
     @JsonProperty("id")
     private String id;
-    @JsonProperty("login")
-    private String login;
-    @JsonProperty("display_name")
-    private String displayName;
-    @JsonProperty("type")
-    private String type;
-    @JsonProperty("broadcaster_type")
-    private String broadcasterType;
-    @JsonProperty("description")
-    private String description;
     @JsonProperty("profile_image_url")
     private String profileImageUrl;
-    @JsonProperty("offline_image_url")
-    private String offlineImageUrl;
-    @JsonProperty("created_at")
-    private String createdAt;
 }
