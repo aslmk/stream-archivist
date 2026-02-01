bbeefaef5f836505af0f58cb4d2c3f2b108e8a04
aslmk <aulaensov@gmail.com>
2026-02-01 19:22:20 +0500

    refactor: remove obsolete StreamerResolutionService and tests

--- tracker-service/src/main/java/com/aslmk/trackerservice/service/StreamerResolutionService.java
+++
@@ -1,7 +0,0 @@
-package com.aslmk.trackerservice.service;
-
-import java.util.UUID;
-
-public interface StreamerResolutionService {
-    UUID resolveStreamerId(String providerUserId, String providerName);
-}
