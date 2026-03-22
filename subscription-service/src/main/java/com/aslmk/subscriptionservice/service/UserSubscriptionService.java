91e2c94774039835c0aac6c87e38e3d6ccfe5b2e
aslmk <aulaensov@gmail.com>
2026-03-22 19:15:06 +0500

    chore: cleanup unused import

--- subscription-service/src/main/java/com/aslmk/subscriptionservice/service/UserSubscriptionService.java
+++ subscription-service/src/main/java/com/aslmk/subscriptionservice/service/UserSubscriptionService.java
@@ -2,7 +2,6 @@ package com.aslmk.subscriptionservice.service;
 
 import com.aslmk.subscriptionservice.dto.CreateUserSubscription;
 import com.aslmk.subscriptionservice.dto.UserSubscriptionsResponse;
-import com.aslmk.subscriptionservice.entity.UserSubscriptionEntity;
 
 public interface UserSubscriptionService {
     UserSubscriptionsResponse getAllUserSubscriptions(String userId);
