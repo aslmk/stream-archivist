dd01569854e39c1faa5221093e8854e13c1014e9
aslmk <aulaensov@gmail.com>
2026-03-30 12:45:47 +0500

    refactor: remove unused StreamRecordingException

--- recording-worker/src/main/java/com/aslmk/recordingworker/exception/StreamRecordingException.java
+++
@@ -1,7 +0,0 @@
-package com.aslmk.recordingworker.exception;
-
-public class StreamRecordingException extends RuntimeException {
-    public StreamRecordingException(String message) {
-        super(message);
-    }
-}
