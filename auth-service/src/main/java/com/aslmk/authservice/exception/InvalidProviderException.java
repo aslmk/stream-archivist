c55f84eacf465407c23f98841bd2621bef7930bd
aslmk <aulaensov@gmail.com>
2026-04-08 09:55:16 +0500

    refactor(exceptions): remove unused exceptions and redundant constructor
    - cleanup exceptions across the system
    - remove unused constructor in SubscriptionServiceClientException

--- auth-service/src/main/java/com/aslmk/authservice/exception/InvalidProviderException.java
+++
@@ -1,7 +0,0 @@
-package com.aslmk.authservice.exception;
-
-public class InvalidProviderException extends RuntimeException {
-    public InvalidProviderException(String message) {
-        super(message);
-    }
-}
