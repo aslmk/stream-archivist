e317de7b9d4a1c2e47c111392434d26f59971fea
aslmk <aulaensov@gmail.com>
2026-04-13 09:50:12 +0500

    feat(storage): remove Kafka integration

--- storage-service/src/main/java/com/aslmk/storageservice/exception/KafkaEventDeserializationException.java
+++
@@ -1,7 +0,0 @@
-package com.aslmk.storageservice.exception;
-
-public class KafkaEventDeserializationException extends RuntimeException {
-    public KafkaEventDeserializationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
