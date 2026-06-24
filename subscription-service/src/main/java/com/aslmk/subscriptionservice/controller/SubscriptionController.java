29688ec74193ba5d4f138dcc9371b4152cb74c7f
aslmk <aulaensov@gmail.com>
2026-06-24 23:54:08 +0500

    fix: replace @NotEmpty with @NotNull for UUID request parameters

--- subscription-service/src/main/java/com/aslmk/subscriptionservice/controller/SubscriptionController.java
+++ subscription-service/src/main/java/com/aslmk/subscriptionservice/controller/SubscriptionController.java
@@ -8,7 +8,7 @@ import com.aslmk.subscriptionservice.dto.UserSubscriptionsResponse;
 import com.aslmk.subscriptionservice.service.SubscriptionOrchestrator;
 import com.aslmk.subscriptionservice.service.UserSubscriptionService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,34 +23,32 @@ public class SubscriptionController {
     private final SubscriptionOrchestrator orchestrator;
     private final UserSubscriptionService service;
 
-    public SubscriptionController(SubscriptionOrchestrator orchestrator, UserSubscriptionService service) {
+    public SubscriptionController(SubscriptionOrchestrator orchestrator,
+                                  UserSubscriptionService service) {
         this.orchestrator = orchestrator;
         this.service = service;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void subscribe(@NotEmpty @RequestHeader(GatewayHeaders.USER_ID) UUID userId,
+    public void subscribe(@NotNull @RequestHeader(GatewayHeaders.USER_ID) UUID userId,
                           @RequestBody @Valid SubscriptionRequest subscriptionRequest) {
         UserRef userRef = new UserRef(userId);
         StreamerRef streamerRef = new StreamerRef(subscriptionRequest.getStreamerUsername(),
                 subscriptionRequest.getProviderName());
-
         orchestrator.subscribe(userRef, streamerRef);
     }
 
     @GetMapping
     public UserSubscriptionsResponse getUserSubscriptions(
-            @NotEmpty @RequestHeader(GatewayHeaders.USER_ID) UUID userId) {
-
+            @NotNull @RequestHeader(GatewayHeaders.USER_ID) UUID userId) {
         return service.getAllUserSubscriptions(userId);
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void unsubscribe(@NotEmpty @RequestHeader(GatewayHeaders.USER_ID) UUID userId,
-                                            @NotEmpty @RequestParam(name = "streamerId") UUID streamerId) {
-
+    public void unsubscribe(@NotNull @RequestHeader(GatewayHeaders.USER_ID) UUID userId,
+                            @NotNull @RequestParam(name = "streamerId") UUID streamerId) {
         orchestrator.unsubscribe(userId, streamerId);
     }
 }
